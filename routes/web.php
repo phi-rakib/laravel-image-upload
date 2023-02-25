3b2ae8065bbfb6644373adc4e40e314130acdd04
rakib <phi.rakib@gmail.com>
2023-02-25 09:57:00 +0600

    added web route for image upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(ImageController::class)->group(function() {
+    Route::get('upload-image', 'index');
+    Route::post('upload-image', 'store')->name('image.store');
 });
